50a15087db1717664c96165e31d214908c109f13
unknown <EDSG@HC-mining.Edostate.gov.ng>
2024-11-03 20:58:22 +0100

    done

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	//var ages = [3]int{22, 23, 21}
+	// greeting := "hello there friends"
+	// fmt.Println(strings.Index(greeting, "th"))
+	//fmt.Println(strings.Split(greeting, "fr"))
 
-	name := [4]string{"jake", "make", "hhs", "yimde"}
+	// fmt.Println(strings.Contains(greeting, "helllo"))
+	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi")) //returns a new string doest alter the old one
+	// fmt.Println(strings.ToUpper(greeting))
 
-	fmt.Println(name)
+	age := []int{32, 45, 67, 32, 34, 56}
+	sort.Ints(age)
+	fmt.Println(age)
 
-	//slices
-	var scores = []int{100, 45, 67, 54}
-	scores[2] = 44
-	scores = append(scores, 77)
-	fmt.Println(scores)
+	index := sort.SearchInts(age, 67)
+	fmt.Println(index)
 
-	//slice ranges
-	rangeOne := name[1:3]
+	names := []string{"jake", "jakee", "make"} //returns alphabetical order
 
-	fmt.Println(rangeOne)
+	sort.Strings(names)
+	fmt.Println(names)
+
 }
