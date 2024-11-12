303b9e4ac8c5bc8c0867b17c45cc5bfc7d94b16b
unknown <EDSG@HC-mining.Edostate.gov.ng>
2024-11-12 10:53:48 +0100

    ddod

--- main.go
+++ main.go
@@ -1,29 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
-	// greeting := "hello there friends"
-	// fmt.Println(strings.Index(greeting, "th"))
-	//fmt.Println(strings.Split(greeting, "fr"))
+	//a simple loop
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++
+	// }
 
-	// fmt.Println(strings.Contains(greeting, "helllo"))
-	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi")) //returns a new string doest alter the old one
-	// fmt.Println(strings.ToUpper(greeting))
-
-	age := []int{32, 45, 67, 32, 34, 56}
-	sort.Ints(age)
-	fmt.Println(age)
-
-	index := sort.SearchInts(age, 67)
-	fmt.Println(index)
-
-	names := []string{"jake", "jakee", "make"} //returns alphabetical order
-
-	sort.Strings(names)
-	fmt.Println(names)
-
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is:", i)
+	}
 }
